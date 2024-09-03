664159140fc570dbf79c008e184176599802f320
Ahmed Adel <dola9240@gmail.com>
2024-09-03 03:21:12 +0300

    first commit

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;   //entity >> Ui بتعبر عن الداتا اللي في ال  
+  final num price;
+  final num rating;          
+
+  BookEntity(this.image, this.title, this.authorName, this.price, this.rating);
+}
