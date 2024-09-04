133eaf3210997e4a247513eb97d4c243295645f1
Ahmed Adel <dola9240@gmail.com>
2024-09-04 18:23:01 +0300

    refactor to use case  class

--- lib/Features/home/domain/use_cases/fetchFeaturedBooks_use_case.dart
+++ lib/Features/home/domain/use_cases/fetchFeaturedBooks_use_case.dart
@@ -1,16 +1,17 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchfeaturedbooksUseCase {
+class FetchfeaturedbooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchfeaturedbooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-
-    // check premission
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
---
+++ lib/core/use_cases/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, param> {
+  Future<Either<Failure, Type>> call([param param]); //optional [argument]
+}
+
+class NoParam {}
